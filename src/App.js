b68bc47344026af505ab483d75755678b3f965e7
Kevin <kemcnulty@csumb.edu>
2023-03-21 09:14:17 -0700

    created route & updated app.js, updateds semester.js, created AddStudentForm in components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import './App.css';
 import AppBar from '@mui/material/AppBar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import SchedList from './components/SchedList';
 import Semester from './components/Semester';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import AddStudentForm from './components/AddStudentForm';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
+  const handleAddStudent = (newStudent) => {
+
+  };
+
   return (
     <div className="App">
-      <AppBar position="static" color="default">
-        <Toolbar>
-           <Typography variant="h6" color="inherit">
-            Course Registration
-           </Typography>
-        </Toolbar>
-      </AppBar>
-      <BrowserRouter>
-       <Switch>
-        <Route exact path='/' component={Semester} />
-        <Route path='/schedule' component={SchedList} />
-       </Switch>
-      </BrowserRouter>
+      <Router>
+        <AppBar position="static" color="default">
+          <Toolbar>
+             <Typography variant="h6" color="inherit">
+              Course Registration
+             </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route exact path="/" component={Semester} />
+          <Route path="/add-student" render={() => <AddStudentForm addStudent={handleAddStudent} />} />     
+          <Route path="/schedule" component={SchedList} />
+        </Switch>
+      </Router>
+	 <ToastContainer />
     </div>
   );
 }
 
 export default App;
+
